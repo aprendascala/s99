d1b5ed7f9643b8000248358d64ed25a0374a0bb8
Jonas Abreu <jonas@vidageek.net>
2012-04-25 14:56:46 -0300

    mudando nome dos testes

--- test/br/com/aprendascala/s99/ex2/ExercicioTest.scala
+++ test/br/com/aprendascala/s99/ex2/ExercicioTest.scala
@@ -7,7 +7,7 @@ import Exercicio._
 class ExercicioTest {
 
   @Test
-  def deveDevolverOUltimoElementoDaLista = {
+  def deveDevolverOPenultimoElementoDaLista = {
     Assert.assertEquals(7, penultimo(List(1, 2, 3, 4, 5, 6, 7, 8)));
     Assert.assertEquals(5, penultimo(List(1, 2, 3, 4, 5, 6)));
     Assert.assertEquals(1, penultimo(List(1, 2)));
--- test/br/com/aprendascala/s99/ex3/ExercicioTest.scala
+++ test/br/com/aprendascala/s99/ex3/ExercicioTest.scala
@@ -7,7 +7,7 @@ import Exercicio._
 class ExercicioTest {
 
   @Test
-  def deveDevolverOUltimoElementoDaLista = {
+  def deveDevolverOEnesimoElementoDaLista = {
     Assert.assertEquals(3, enesimo(2, List(1, 2, 3, 4, 5, 6, 7, 8)));
     Assert.assertEquals(6, enesimo(5, List(1, 2, 3, 4, 5, 6)));
     Assert.assertEquals(1, enesimo(3, List(5, 3, 2, 1)));
