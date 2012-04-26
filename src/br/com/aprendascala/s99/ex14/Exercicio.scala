8361085877a29d2385d28908111a4e5eb3411bef
Jonas Abreu <jonas@vidageek.net>
2012-04-26 00:27:48 -0300

    adicionado exercicio 14

---
+++ src/br/com/aprendascala/s99/ex14/Exercicio.scala
@@ -0,0 +1,7 @@
+package br.com.aprendascala.s99.ex14
+
+object Exercicio {
+
+  def duplicate(list : List[Symbol]) = {
+  }
+}
---
+++ test/br/com/aprendascala/s99/ex14/ExercicioTest.scala
@@ -0,0 +1,17 @@
+package br.com.aprendascala.s99.ex14;
+
+import org.junit.Test;
+import org.junit.Assert
+
+import Exercicio._
+
+class ExercicioTest {
+
+  @Test
+  def deveEmpacotarALista = {
+    Assert.assertEquals(List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd),
+      duplicate(List('a, 'b, 'c, 'c, 'd)))
+    Assert.assertEquals(List('a, 'a), duplicate(List('a)))
+  }
+
+}
