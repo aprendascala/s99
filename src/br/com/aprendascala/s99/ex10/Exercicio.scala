31cc3b3a983fdcc3b8d7085d116ef45e5e16a73f
Jonas Abreu <jonas@vidageek.net>
2012-04-25 21:10:18 -0300

    adicionado exercicio 10

---
+++ src/br/com/aprendascala/s99/ex10/Exercicio.scala
@@ -0,0 +1,8 @@
+package br.com.aprendascala.s99.ex10
+
+object Exercicio {
+
+  def encode(list : List[Symbol]) = {
+
+  }
+}
---
+++ test/br/com/aprendascala/s99/ex10/ExercicioTest.scala
@@ -0,0 +1,16 @@
+package br.com.aprendascala.s99.ex10;
+
+import org.junit.Test;
+import org.junit.Assert
+import Exercicio._
+
+class ExercicioTest {
+
+  @Test
+  def deveEmpacotarALista = {
+    Assert.assertEquals(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)),
+      encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)))
+    Assert.assertEquals(List((1, 'a), (1, 'b), (1, 'c), (1, 'g)), encode(List('a, 'b, 'c, 'g)))
+  }
+
+}
